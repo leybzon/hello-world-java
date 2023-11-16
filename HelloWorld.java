4367fb4a728901f2ed18eb66afd593ed1469ff98
Gene Leybzon <yevgeniy.leybzon@globallogic.com>
2023-11-15 16:12:06 -0800

    Added arguments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    if (args.length > 0) {
+      for (String arg : args) {
+        System.out.println("Hello, " + arg);
+      }
+    } else {
+      System.out.println("Hello Java world!");
+    }
   }
 }
