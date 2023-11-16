3dec542a0e0972d4f2ddd77de92abc1eb1fb5422
Gene Leybzon <yevgeniy.leybzon@globallogic.com>
2023-11-16 08:47:14 -0800

    Refactored code for better performance.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,78 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class HelloWorld {
+  private static final int numberOfIntegers = 5;
   //integer array to store the positive numbers
-  int[] positiveNumbers = {1, 2, 3, 4, 5};
+  int[] positiveNumbers = new int[numberOfIntegers];
   //double array to store the negative numbers
   double[] negativeNumbers = {-1.0, -2.0, -3.0, -4.0, -5.0};
   
+  public class PositiveNumberSorter {
+    private static final int numberOfIntegers = 5;
+    private static final int RANDOM_UPPER_BOUND = 100;
+    //integer array to store the positive numbers
+    int[] positiveNumbers = new int[numberOfIntegers];
+    //double array to store the negative numbers
+    double[] negativeNumbers = {-1.0, -2.0, -3.0, -4.0, -5.0};
+
+    public PositiveNumberSorter() {
+      Random random = new Random();
+      for (int i = 0; i < positiveNumbers.length; i++) {
+          positiveNumbers[i] = random.nextInt(RANDOM_UPPER_BOUND); // generates random numbers between 0 and 99
+      }
+    }
+
+    //function to sort the positive numbers
+    public void sortPositiveNumbers() {
+      Arrays.sort(positiveNumbers);
+    }
+
+    //function to sort the positive numbers in reverse order
+    public void sortPositiveNumbersInReverse() {
+      List<Integer> list = Arrays.stream(positiveNumbers).boxed().collect(Collectors.toList());
+      Collections.sort(list);
+      Collections.reverse(list);
+      positiveNumbers = list.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
+    //function to print the positive numbers
+    public void printPositiveNumbers() {
+      for (int number : positiveNumbers) {
+          System.out.println(number);
+      }
+    }
+
+    public static void main(String[] args) {
+      HelloWorld helloWorld = new HelloWorld();
+      PositiveNumberSorter positiveNumberSorter = helloWorld.new PositiveNumberSorter();
+      positiveNumberSorter.sortPositiveNumbers();
+      positiveNumberSorter.printPositiveNumbers();
+      System.out.println("Hello developers!");
+      positiveNumberSorter.sortPositiveNumbersInReverse();
+      positiveNumberSorter.printPositiveNumbers();
+    }
+  }
+
+  //function to sort the positive numbers
+  public void sortPositiveNumbers() {
+    Arrays.sort(positiveNumbers);
+  }
+
   //function to sort the positive numbers in reverse order
   public void sortPositiveNumbersInReverse() {
     List<Integer> list = Arrays.stream(positiveNumbers).boxed().collect(Collectors.toList());
     Collections.sort(list);
     Collections.reverse(list);
     positiveNumbers = list.stream().mapToInt(Integer::intValue).toArray();
-  }
-
-  public void sortPositiveNumbers() {
-    Arrays.sort(positiveNumbers);
   }
   
   //function to print the positive numbers
