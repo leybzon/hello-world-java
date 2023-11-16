2c62b8f86bd4daef64c6ef3bc518fadfb8e0ba9b
Gene Leybzon <yevgeniy.leybzon@globallogic.com>
2023-11-15 16:15:52 -0800

    Changed message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    if (args.length > 0) {
-      for (String arg : args) {
-        System.out.println("Hello, " + arg);
-      }
-    } else {
-      System.out.println("Hello Java world!");
-    }
+    System.out.println("Hello developers!");
   }
 }
